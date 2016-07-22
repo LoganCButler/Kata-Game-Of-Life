545af07c08eae1f19588e3679f3085a27e30e0f4
Logan Butler <logan.butler@agilethought.com>
2016-07-22 10:19:48 -0400

    initial commit

---
+++ GameOfLife/ApplicationLogic/App.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationLogic
+{
+    public class GameOfLifeRules
+    {
+        int[,] GameOfLifeBoard;
+
+        public int[,] RunLifeCycle(int[,] currentboard)
+        {
+            GameOfLifeBoard = currentboard;
+            var boardCoppy = currentboard;
+            var nextBoard = currentboard;
+
+            var responce = GetNewCellState(CellState.Alive, getLiveNeighborsNumber(1,1));
+
+            if (responce == CellState.Dead)
+            {
+                nextBoard[1, 1] = 0;
+            }
+
+
+            return nextBoard;
+        }
+
+        private int getLiveNeighborsNumber(int xlocation, int ylocation)
+        {
+            int liveNeighborCount = 0;
+
+            var north = GameOfLifeBoard[xlocation, ylocation - 1]; //north
+            var south = GameOfLifeBoard[xlocation, ylocation + 1]; //south
+            var west =  GameOfLifeBoard[xlocation - 1, ylocation]; //West
+            var east = GameOfLifeBoard[xlocation + 1, ylocation]; //East
+
+            if (north == 1) { liveNeighborCount += 1; };
+            if (south == 1) { liveNeighborCount += 1; };
+            if (west == 1) { liveNeighborCount += 1; };
+            if (east == 1) { liveNeighborCount += 1; };
+
+
+            return liveNeighborCount;
+        }
+
+        public CellState GetNewCellState(CellState currentState, int liveNeighbors)
+        {
+            if (currentState == CellState.Alive)
+            {
+                switch (liveNeighbors)
+                {
+                    case 2:
+                    case 3:
+                        return CellState.Alive;
+                    default:
+                        return CellState.Dead;
+                }
+            }
+            else if (liveNeighbors == 3)
+                return CellState.Alive;
+
+
+            return currentState;
+        }
+
+
+
+        public enum CellState
+        {
+            Dead = 0,
+            Alive = 1,         
+        }
+    }
+}
---
+++ GameOfLife/UnitTest/UnitTest1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ApplicationLogic;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class NeighborTest
+    {
+        [TestMethod]
+        public void NoNeighbors()
+        {
+            //Arrange
+            GameOfLifeRules GameOfLife = new GameOfLifeRules();
+            int[,] currentboard = { { 0, 0, 0 }, {0,1,0 }, {0,0,0 } };
+
+            //Act
+
+            var answer = GameOfLife.RunLifeCycle(currentboard);
+
+            //Assert
+            int[,] expectedEndBoard = { { 0, 0, 0 }, { 0, 0, 0 }, { 0, 0, 0 } };
+            Assert.AreEqual(expectedEndBoard[1,1], answer[1,1]);
+        }
+    }
+
+    [TestClass]
+    public class GameOfLifeCellStateRules
+    {      
+        [TestMethod]
+        public void FewerThanAlive2Dies() //Any live cell with fewer than two live neighbours dies, as if caused by under-population.
+        {
+            //Arrange
+            GameOfLifeRules GameOfLife = new GameOfLifeRules();          
+            var currentState = GameOfLifeRules.CellState.Alive;
+            int liveNeighbors = 1; // underpopulated value
+
+            //Act
+            var result = GameOfLife.GetNewCellState(currentState, liveNeighbors);
+
+            //Assert
+            Assert.AreEqual(GameOfLifeRules.CellState.Dead, result); //Assert that cell dies appropiatly 
+        }
+
+
+        [TestMethod]
+        public void CellLivesWith2() // Any live cell with two live neighbours lives on to the next generation.
+        {
+            //Arrange
+            GameOfLifeRules GameOfLife = new GameOfLifeRules();
+            var currentState = GameOfLifeRules.CellState.Alive;
+            int liveNeighbors = 2; // stable Population value
+
+            //Act
+            var result = GameOfLife.GetNewCellState(currentState, liveNeighbors);
+
+            //Assert
+            Assert.AreEqual(GameOfLifeRules.CellState.Alive, result); //Assert that lives 
+        }
+
+
+        [TestMethod]
+        public void CellLivesWith3() // Any live cell with three live neighbours lives on to the next generation.
+        {
+            //Arrange
+            GameOfLifeRules GameOfLife = new GameOfLifeRules();
+            var currentState = GameOfLifeRules.CellState.Alive;
+            int liveNeighbors = 3; // stable Population value
+
+            //Act
+            var result = GameOfLife.GetNewCellState(currentState, liveNeighbors);
+
+            //Assert
+            Assert.AreEqual(GameOfLifeRules.CellState.Alive, result); //Assert that lives 
+        }
+
+        [TestMethod]
+        public void CellLivesWiteMoreThan3() // Any live cell with more than three live neighbours dies, as if by over-population.
+        {
+            //Arrange
+            GameOfLifeRules GameOfLife = new GameOfLifeRules();
+            var currentState = GameOfLifeRules.CellState.Alive;
+            int liveNeighbors = 5; // over Population value
+
+            //Act
+            var result = GameOfLife.GetNewCellState(currentState, liveNeighbors);
+
+            //Assert
+            Assert.AreEqual(GameOfLifeRules.CellState.Dead, result); //Assert that Dies 
+        }
+
+        [TestMethod]
+        public void CellBornWithe3() // Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+        {
+            //Arrange
+            GameOfLifeRules GameOfLife = new GameOfLifeRules();
+            var currentState = GameOfLifeRules.CellState.Dead;
+            int liveNeighbors = 3; // Reproduction
+
+            //Act
+            var result = GameOfLife.GetNewCellState(currentState, liveNeighbors);
+
+            //Assert
+            Assert.AreEqual(GameOfLifeRules.CellState.Alive, result); //Assert that Cell is born 
+        }
+
+        [TestMethod]
+        public void CellDeadStaysDead() //Dead and stays dead
+        {
+            //Arrange
+            GameOfLifeRules GameOfLife = new GameOfLifeRules();
+            var currentState = GameOfLifeRules.CellState.Dead;
+            int liveNeighbors = 4; // stays dead
+
+            //Act
+            var result = GameOfLife.GetNewCellState(currentState, liveNeighbors);
+
+            //Assert
+            Assert.AreEqual(GameOfLifeRules.CellState.Dead, result); //Assert that Cell is dead 
+        }
+
+
+
+
+
+
+    }
+}
